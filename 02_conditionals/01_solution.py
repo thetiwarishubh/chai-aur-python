b8056cadf373070c54402eb3ad62c4b52768a8a6
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2025-11-22 02:58:07 +0530

    conditionals

---
+++ 02_conditionals/01_solution.py
@@ -0,0 +1,10 @@
+user_age = 45
+
+if user_age < 13 : 
+    print("Child")
+elif user_age < 20 :
+    print("Teenager")
+elif user_age < 60 : 
+    print("Adult")
+else :
+    print("Senior")
---
+++ 02_conditionals/02_solutions.py
@@ -0,0 +1,7 @@
+user_Age = 26
+day = "Monday"
+price = 12 if user_Age >= 18 else 8
+
+if day == "Wednesday" : 
+    price -= 2
+    print("Ticket price for you $", price)
