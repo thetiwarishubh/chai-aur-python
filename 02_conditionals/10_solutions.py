faa83993ac01e30739f2646bb02d9d9cf080205d
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2025-11-23 22:19:11 +0530

    update

--- 02_conditionals/10_solutions.py
+++ 02_conditionals/10_solutions.py
@@ -1,10 +1,16 @@
-animal = "dog"
-year = 1
+animal = "cat"
+year = 2
 
-if year < 2 :
-    if animal == "dog" :
+if animal == "dog":
+    if year < 2:
         print("Puppy Food")
-    else :
-        print("Not Food")
-elif year > 5 :
-    print("Senior cat Food")
+    else:
+        print("Adult Dog Food")
+
+elif animal == "cat":
+    if year < 2:
+        print("Kitten Food")
+    elif year > 5:
+        print("Senior Cat Food")
+    else:
+        print("Adult Cat Food")
