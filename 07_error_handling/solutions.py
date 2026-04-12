3ff4198a893d05b47bbdbfdbde2d1524986a2dcd
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2026-04-13 00:33:04 +0530

    error handling

---
+++ 07_error_handling/solutions.py
@@ -0,0 +1,22 @@
+# try :
+#     num = int(input("Please enter number ? "))
+#     result = 20/num
+#     print(round(result, 1))  
+# except ZeroDivisionError:
+#     print("Error : Division by zero is not allowed")
+# except ValueError:
+#     print("Error : Please enter a valid integer")
+
+
+
+
+
+age = int(input("Please enter your age ? "))
+
+try :
+    if age < 10 or age > 18 :
+        raise ValueError("You'are not eligible for the club")
+    else :
+        print("Welcome to the club")
+except  Exception as Error:
+    print(f"Got and {Error}")
