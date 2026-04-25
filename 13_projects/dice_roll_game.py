540a30e0d0bf9a23bf22cc33a7eb6721a1c15e48
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2026-04-25 14:41:47 +0530

    add count in dice game

--- 13_projects/dice_roll_game.py
+++ 13_projects/dice_roll_game.py
@@ -1,12 +1,15 @@
 import random
 
+dice_count = 0
 while True : 
     choice = input("Do you want to play (Y/N) ?").lower()
 
     if choice == "y" :
         print(f"({random.randint(1, 6)}, {random.randint(1, 6)})")
+        dice_count += 1
     elif choice == "n" :
         print("Thank You for Playing!")
+        print(dice_count)
         break
     else :
         print("Invalid Syntax!")
