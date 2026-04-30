b0d3c8424ca949a90df14f973610cab14941db90
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2026-05-01 01:12:20 +0530

    string

---
+++ 13_string/solution.py
@@ -0,0 +1,7 @@
+# In Python, a string is an immutable sequence of Unicode characters used to store and manipulate text.  The formal type name is str, and strings are created by enclosing characters in single quotes ('), double quotes ("), or triple quotes (''' or """ for multi-line text)
+
+
+user = "Shubham"
+uppercase = user.upper()
+print(uppercase)
+print(user.upper())
