4cef24ccafd57700c2b824d8164b7e7f8020368b
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2026-05-03 21:46:57 +0530

    add features

--- 14_projects/number_guessing_game.py
+++ 14_projects/number_guessing_game.py
@@ -4,6 +4,11 @@ guess = random.randint(1, 10)
 count = 0
 while True :
     choice = int(input("Guess the number ? "))
+
+    if choice > 10 :
+        print("Invalid number! Please enter 1 to 10")
+        break
+
     count += 1
     if choice > guess :
         print("Number is too high")
