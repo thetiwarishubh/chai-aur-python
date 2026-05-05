16db15cb68c9307e486b8d89f59a7b265da55dbb
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2026-05-05 11:58:58 +0530

    qr code generator

---
+++ 14_projects/qr_code_generator.py
@@ -0,0 +1,16 @@
+import qrcode
+
+qr = qrcode.QRCode(
+    version=1, 
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+data = input("Please Enter your portfolio url ? ") 
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+
+img.save("qrcode.png")   
