32ff04bb8b1552c4c54c8e6fd803f3f330dc375f
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2026-05-07 01:37:57 +0530

    some questions

---
+++ 15_practice_questions/solutions.py
@@ -0,0 +1,32 @@
+# print 1 - 100
+
+for x in range(1, 101) :
+    # print(x)
+    pass
+
+
+# sum of 1 to N
+
+sum = 0
+for x in range(1, 101):
+    sum += x
+
+# print(sum)
+
+# Print Multiplication table (user input)
+
+num = int(input("Please enter number ? "))
+
+for x in range(1, 11) :
+    print(f"{num} * {x} = {num * x}")
+
+
+# Factorial of a number
+
+fact = int(input("Please enter a num ? "))
+fact_num = 1
+for x in range(fact , 0, -1) :
+    fact_num *=  x
+
+
+print(fact_num)
