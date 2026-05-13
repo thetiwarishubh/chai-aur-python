b6fea26b6f1fb9d2c5e5fa0704cf03abeb710e0a
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2026-05-13 07:34:09 +0530

    understand lambda funtion

--- 04_functions/lambda.py
+++ 04_functions/lambda.py
@@ -1,13 +1,13 @@
 # Global variable
 x = 10
 
-def modify_var():
+# def modify_var():
     
-    global x # use global to make a varibale global
-    x = 20
+#     global x # use global to make a varibale global
+#     x = 20
 
-modify_var()
-print(x)
+# modify_var()
+# print(x)
 
 
 # lambda arguments: expression
@@ -15,3 +15,6 @@ print(x)
 
 # A lambda function is a small anonymous function.
 # A lambda function can take any number of arguments, but can only have one expression.
+
+check = lambda x: "Positive" if x > 0 else "Negative"
+print(check(-1))
